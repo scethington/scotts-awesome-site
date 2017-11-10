e9b5c502dd8379580674d2ef5c10c6e641154a81
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2017-11-10 12:17:39 -0500

    code clean up and presskit
    added presskit page. code clean up of unused images

---
+++ src/container/press-kit/press-kit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'press-kit',
+    templateUrl: './press-kit.component.html',
+    styleUrls: ['./press-kit.component.css']
+  })
+  export class PressKitComponent implements OnInit {
+  
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  }
+  
